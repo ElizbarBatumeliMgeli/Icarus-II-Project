3e99305169cd708aea9032563c51bcba084eac8b
Elizbar_kheladze <kheladzee2001@gmail.com>
2026-05-18 12:00:34 +0200

    Initial Commit

---
+++ Icarus II Project/Icarus_II_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Icarus_II_ProjectApp.swift
+//  Icarus II Project
+//
+//  Created by Elizbar Kheladze on 18/05/26.
+//
+
+import SwiftUI
+
+@main
+struct Icarus_II_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
