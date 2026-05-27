bb68c7aea0b4de5218b07f21c4635539bb66befb
Aleksandra <aleksandra.stupiec@gmail.com>
2026-05-27 10:16:02 +0200

    changed match model, made view models to follow mvvm arch

---
+++ Icarus II Project/ViewModels/CardViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  CardViewModel.swift
+//  Icarus II Project
+//
+//  Created by Aleksandra Stupiec on 26/05/26.
+//
+
+import SwiftUI
+
+struct CardViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardViewModel()
+}
---
+++ Icarus II Project/ViewModels/MatchViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  MatchViewModel.swift
+//  Icarus II Project
+//
+//  Created by Aleksandra Stupiec on 26/05/26.
+//
+
+import SwiftUI
+
+struct MatchViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MatchViewModel()
+}
